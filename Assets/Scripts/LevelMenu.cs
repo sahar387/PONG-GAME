4e0fc9f0c6638644e79d405bc92919dccdab1a1a
sahar387 <sahareftekhari680@gmail.com>
2024-11-26 23:54:46 +0330

    ADD: Assets and Scripts

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelsOpen : MonoBehaviour
+public class LevelMenu : MonoBehaviour
 {
-  
     public void OpenLevel(int levelId)
     {
         string levelName = "Level" + levelId;
